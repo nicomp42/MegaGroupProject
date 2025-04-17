91697e1f2a7a4f3fa3bf270cc5ba8b05c5abbcab
Caitlin Hutchins <hutchicu@mail.uc.edu>
2025-04-17 12:56:45 -0400

    hutchicu addition

---
+++ MegaGroupProject/groupPackage/hutchicu.py
@@ -0,0 +1,10 @@
+class GroupCollaboration:
+    """
+    This class is created for the group collaboration assignment.
+    """
+
+    def get_favorite_food(self):
+        """
+        Returns the favorite food as a string.
+        """
+        return "ice cream"
