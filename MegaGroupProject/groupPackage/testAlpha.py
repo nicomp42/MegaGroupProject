1457253bbc92ea7485eca713877344648870ce8e
nicomp42 <nicholdw@ucmail.uc.edu>
2025-04-17 07:56:38 -0400

    Cleanup

--- MegaGroupProject/groupPackage/testAlpha.py
+++ MegaGroupProject/groupPackage/testAlpha.py
@@ -1,3 +1,4 @@
 
 def print_me():
     print("testAlpha.py")
+ 
--- MegaGroupProject/mainPackage/main.py
+++ MegaGroupProject/mainPackage/main.py
@@ -14,14 +14,32 @@ if __name__ == "__main__":
     #for module in modules:
     #    print(module)
 
-    expected_function = "print_me"   # No parens should be used here
+    expected_function = "get_favorite_food"   # No parens should be used here
+    favorite_foods = dict()
+    success_count = 0
+    failure_count = 0
     for module in modules:
         #print(module)
         attributes = sorted(dir(module))
+        module_name = module.__str__().split("\\\\")[-1][:-2]
         #if attributes:
         #    for attribute in attributes:
         #        print(attribute)
-        function = getattr(module, expected_function)
-        if callable(function):
-            print("Executing", expected_function + "()", "in", module.__str__().split("\\\\")[-1][:-2])
-            function()
+        try:
+            function = getattr(module, expected_function)
+            if callable(function):
+                print("Executing", expected_function + "()", "in", module.__str__().split("\\\\")[-1][:-2])
+                try:
+                    favorite_food = getattr(module, expected_function)()
+                    favorite_foods[module_name] = favorite_food
+                    success_count = success_count + 1
+                    #print("  favorite food is", favorite_food)
+                except Exception as e:
+                    print(e)
+                    failure_count = failure_count + 1
+        except Exception as e:
+            print(e)
+            failure_count = failure_count + 1
+    print("Success count: ", success_count, "Failure count:", failure_count)
+    print("Favorite Foods:")
+    print(favorite_foods)
