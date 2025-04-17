d4ea04db2dcd910591fd06a8be862f5b3668387b
Zulqarnayan Hossain <hossainzulqarnayan@gmail.com>
2025-04-17 12:54:47 -0400

    hossaizn fav food is 'Biriyani'!

---
+++ MegaGroupProject/groupPackage/hossaizn.py
@@ -0,0 +1,17 @@
+# File Name : hossaizn.py
+# Student Name: Zulqarnayan Hossain
+# email:  hossaizn@mail.uc.edu
+# Assignment Number: In-Class Assignment 04-17-2025
+# Due Date:   April 17, 2025
+# Course #/Section:   IS 4010 Section 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Github Collaboration using Modularity
+
+# Brief Description of what this module does. Modularity in Coding Structures
+# Citations: N/A
+
+# Anything else that's relevant: N/A
+
+class GroupCollaboration:
+    def get_favorite_food(self):
+        return "Biriyani" 
